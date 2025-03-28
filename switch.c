39a4a471ecc92c667efb7af56777c3cd93f7b6bf
Avishka-42 <avishka4767@gmail.com>
2025-03-28 13:22:11 +0530

    branch4

--- switch.c
+++ switch.c
@@ -14,10 +14,10 @@ int main() {
             printf("Btech cse,at amity university\n");
             break;
 	    case 3:
-            printf("Ece,at amity university\n");
+            printf("Ece,at amity universityti\n");
             break;
 	   case 4:
-            printf("mechanical\n");
+            printf("mechanical,at amity university\n");
             break;
 	    default:
             printf("Invalid choice. Please try again with a number between 1 and 4.\n");
