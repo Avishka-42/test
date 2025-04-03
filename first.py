94a07244eaa7992d8bac3795d17ed3227164f561
Avishka-42 <avishka4767@gmail.com>
2025-04-03 21:12:06 +0530

    program

--- first.py
+++ first.py
@@ -50,3 +50,11 @@ name1 = str(input("enter the name1"))
 name2 = str(input("enter the name2"))
 print("name1 is",name1,"name2 is",name2)
 name1,name2 = name2,name1print("name1 is",name1,"name2 is",name2)
+
+make a simple calculator.
+
+def calculator():
+num1 = float(input("enter first no:"))
+operator = input("enter operator(+,-,*,/):")
+num2 = float(input("enter second no:"))
+match operator:
