ef3bb61f1f9f853f67892a17d72c50da2159d7ea
Avishka-42 <avishka4767@gmail.com>
2025-04-04 11:25:52 +0530

    program1

--- second.py
+++ second.py
@@ -3,6 +3,10 @@ factorization methos.
 def prime_factors(n):
     factors = []
     for i in range(2, n + 1):
-    
+  while n % i == 0:
+            factors.append(i)
+            n //= i
+    return factors
+  
 
 
