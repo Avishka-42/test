67d371b3d8c21e749b9edf316467ea6ed8cd3760
Avishka-42 <avishka4767@gmail.com>
2025-04-06 23:39:17 +0530

    program6

--- second.py
+++ second.py
@@ -78,4 +78,9 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
 
