c7b247b3d2ec86a7518707e48a17c542113d845e
Avishka-42 <avishka4767@gmail.com>
2025-05-23 11:31:50 +0530

    c lang

---
+++ .c
@@ -0,0 +1,18 @@
+case 1:
+            printf("Btech aiml\n");
+            break;
+	    case 2:
+            printf("Btech cse\n");
+            break;
+	    case 3:
+            printf("Ece\n");
+            break;
+	   case 4:
+            printf("mechanical\n");
+            break;
+	    default:
+            printf("Invalid choice. Please try again with a number between 1 and 4.\n");
+            break;
+    }
+
+    return 0;
